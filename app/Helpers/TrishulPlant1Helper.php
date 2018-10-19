66eee84df25addfe3fc62fae66bde0cffbc3e0e0
Rajiv Seelam <rajiv@betalectic.com>
2018-10-19 11:00:20 +0530

    BB-6 Remove BB filter, Order By Batch_Date

--- app/Helpers/TrishulPlant1Helper.php
+++ app/Helpers/TrishulPlant1Helper.php
@@ -15,9 +15,10 @@ class TrishulPlant1Helper {
 		$dbBatches = TrishulPlant1BatchDatTrans::with('transactions','customer')
 										->where('Batch_Date','>=', $fromDate)
 										->where('Batch_Date','<=', $toDate)
-										->whereHas('customer',function($query){
-											return $query->where('Customer_Name', 'LIKE', 'BB-%');
-										})
+										// ->whereHas('customer',function($query){
+										// 	return $query->where('Customer_Name', 'LIKE', 'BB-%');
+										// })
+										->orderBy('Batch_Date','asc')
 										->get();
 
 		$batches = [];
